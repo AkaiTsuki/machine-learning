fe5d6af535b6046a486cee90808fdd8ad3920f5a
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-10-09 14:13:31 -0400

    Implemented Gaussian Naive Bayes.

--- ml-homework/hw3/main_naive_bayes.py
+++ ml-homework/hw3/main_naive_bayes.py
@@ -1,12 +1,12 @@
 __author__ = 'jiachiliu'
 
 from nulearn.dataset import load_spambase
-from nulearn.bayes import BernoulliNaiveBayes
+from nulearn.bayes import *
 from nulearn.cross_validation import *
 from nulearn.validation import *
 
 
-def bernoulliNB():
+def naive_bayes(c):
     train, target = load_spambase()
     train, target = shuffle(train, target)
 
@@ -26,7 +26,7 @@ def bernoulliNB():
         train_target = np.append(target[range(0, start)], target[range(end, train_size)])
         test_target = target[range(start, end)]
 
-        cf = BernoulliNaiveBayes()
+        cf = get_classifier(c)
         predicts = cf.fit(k_fold_train, train_target).predict_class(test)
 
         print '=============Fold %s==================' % fold
@@ -43,5 +43,31 @@ def bernoulliNB():
     print 'Overall Accuracy: %s, Overall Error: %s\n' % (overall_acc/k, overall_error/k)
 
 
+def get_classifier(c):
+    if c == 'bernoulli':
+        return BernoulliNaiveBayes()
+    if c == 'gaussian':
+        return GaussianNaiveBayes()
+
+
+def gaussian_naive_bayes():
+    naive_bayes('gaussian')
+
+
+def bernoulli_naive_bayes():
+    naive_bayes('bernoulli')
+
+
+def gaussian_naive_bayes1():
+    train, target = load_spambase()
+    train, test, train_target, test_target = train_test_shuffle_split(train, target, len(train) / 10)
+    cf = GaussianNaiveBayes()
+    predicts = cf.fit(train, train_target).predict_class(test)
+    cm = confusion_matrix(test_target, predicts)
+    print "confusion matrix: TN: %s, FP: %s, FN: %s, TP: %s" % (cm[0, 0], cm[0, 1], cm[1, 0], cm[1, 1])
+    er, acc, fpr, tpr = confusion_matrix_analysis(cm)
+    print 'Error rate: %f, accuracy: %f, FPR: %f, TPR: %f' % (er, acc, fpr, tpr)
+
 if __name__ == '__main__':
-    bernoulliNB()
+    # bernoulli_naive_bayes()
+    gaussian_naive_bayes()
--- ml-homework/hw3/nulearn/bayes.py
+++ ml-homework/hw3/nulearn/bayes.py
@@ -69,3 +69,74 @@ class BernoulliNaiveBayes:
     @staticmethod
     def get_overall_mean(train):
         return [train[:, f].mean() for f in range(train.shape[1])]
+
+
+class GaussianNaiveBayes:
+    def __init__(self):
+        # the mean vector for all features
+        self.overall_mean = None
+        # the variance vector for all features
+        self.overall_var = None
+        # class conditional mean
+        self.conditional_mean = {}
+        # class conditional var
+        self.conditional_var = {}
+        # all labels
+        self.labels = None
+        self.priors = {}
+
+    def setup(self, train, target):
+        self.overall_mean = self.get_mean_vector(train)
+        self.overall_var = self.get_var_vector(train)
+        self.labels = np.unique(target)
+
+    @staticmethod
+    def get_mean_vector(data):
+        return np.array([data[:, f].mean() for f in range(data.shape[1])])
+
+    @staticmethod
+    def get_var_vector(data):
+        return np.array([data[:, f].var() for f in range(data.shape[1])])
+
+    def fit(self, train, target):
+        self.setup(train, target)
+        n = len(target)
+        p = 1.0 * n / (n + 2)
+        for l in self.labels:
+            tuples = train[target == l]
+            self.priors[l] = 1.0 * len(tuples) / n
+            self.conditional_mean[l] = self.get_mean_vector(tuples)
+            self.conditional_var[l] = p * self.get_var_vector(tuples) + (1 - p) * self.overall_var
+        return self
+
+    def predict(self, test):
+        predicts = []
+        for t in test:
+            res = []
+            for l in self.labels:
+                liklihood = 1.0
+                for f in range(test.shape[1]):
+                    #print "index, feature, mean, var: %s %s %s %s" % (f, t[f],self.get_class_conditional_mean(l, f), self.get_class_conditional_var(l, f))
+                    g = self.gaussian(t[f], self.get_class_conditional_mean(l, f), self.get_class_conditional_var(l, f))
+                    liklihood *= g
+                res.append(liklihood * self.priors[l])
+            predicts.append(res)
+        return predicts
+
+    def predict_class(self, test):
+        predicts = self.predict(test)
+        #print predicts
+        return np.array(map(lambda p: 1.0 if p[0] <= p[1] else 0.0, predicts))
+
+    @staticmethod
+    def gaussian(f, m, v):
+        v2 = ((f - m) * (f - m)) / (2.0 * v)
+        v1 = np.exp(-v2)
+        v3 = v1 / np.sqrt(2.0 * v * np.pi)
+        return v3
+
+    def get_class_conditional_mean(self, label, feature):
+        return self.conditional_mean[label][feature]
+
+    def get_class_conditional_var(self, label, feature):
+        return self.conditional_var[label][feature]
