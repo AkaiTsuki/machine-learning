0b96a676e65c80cc17bcc0daa97a6fab6809cfe3
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-10-09 11:21:55 -0400

    Refactored code, add k-fold-validation on test

--- ml-homework/hw3/main_naive_bayes.py
+++ ml-homework/hw3/main_naive_bayes.py
@@ -2,21 +2,46 @@ __author__ = 'jiachiliu'
 
 from nulearn.dataset import load_spambase
 from nulearn.bayes import BernoulliNaiveBayes
-from nulearn.cross_validation import train_test_shuffle_split
+from nulearn.cross_validation import *
 from nulearn.validation import *
 
 
-train, target = load_spambase()
+def bernoulliNB():
+    train, target = load_spambase()
+    train, target = shuffle(train, target)
 
-overall_means = [train[:, f].mean() for f in range(train.shape[1])]
-overall_std = [train[:, f].std() for f in range(train.shape[1])]
+    k = 10
+    train_size = len(train)
+    test_index_generator = k_fold_cross_validation(train_size, k)
 
-train, test, train_target, test_target = train_test_shuffle_split(train, target, len(train) / 10)
+    fold = 1
 
-cf = BernoulliNaiveBayes(overall_means)
-predicts = cf.fit(train, train_target).predict_class(test)
+    overall_acc = 0
+    overall_error = 0
 
-cm = confusion_matrix(test_target, predicts)
-print "confusion matrix: TN: %s, FP: %s, FN: %s, TP: %s" % (cm[0, 0], cm[0, 1], cm[1, 0], cm[1, 1])
-er, acc, fpr, tpr = confusion_matrix_analysis(cm)
-print 'Error rate: %f, accuracy: %f, FPR: %f, TPR: %f' % (er, acc, fpr, tpr)
+    for start, end in test_index_generator:
+        k_fold_train = np.vstack((train[range(0, start)], train[range(end, train_size)]))
+        test = train[range(start, end)]
+
+        train_target = np.append(target[range(0, start)], target[range(end, train_size)])
+        test_target = target[range(start, end)]
+
+        cf = BernoulliNaiveBayes()
+        predicts = cf.fit(k_fold_train, train_target).predict_class(test)
+
+        print '=============Fold %s==================' % fold
+        cm = confusion_matrix(test_target, predicts)
+        print "confusion matrix: TN: %s, FP: %s, FN: %s, TP: %s" % (cm[0, 0], cm[0, 1], cm[1, 0], cm[1, 1])
+        er, acc, fpr, tpr = confusion_matrix_analysis(cm)
+        print 'Error rate: %f, accuracy: %f, FPR: %f, TPR: %f' % (er, acc, fpr, tpr)
+
+        overall_acc += acc
+        overall_error += er
+        fold += 1
+
+    print '--------------- Result-------------------'
+    print 'Overall Accuracy: %s, Overall Error: %s\n' % (overall_acc/k, overall_error/k)
+
+
+if __name__ == '__main__':
+    bernoulliNB()
--- ml-homework/hw3/nulearn/bayes.py
+++ ml-homework/hw3/nulearn/bayes.py
@@ -4,38 +4,44 @@ import numpy as np
 
 
 class BernoulliNaiveBayes:
-    def __init__(self, overall_mean):
-        self.overall_mean = overall_mean
-        self.probabilities = {}
+    def __init__(self):
+        self.overall_mean = None
+        self.likelihoods = {}
         self.labels = None
-        self.labels_prob = []
+        self.priors = {}
+
+    def setup(self, train, target):
+        self.overall_mean = self.get_overall_mean(train)
+        # find unique class labels
+        self.labels = np.unique(target)
 
     def fit(self, train, target):
-        # find unique class labels
-        labels = np.unique(target).tolist()
-        self.labels = labels
+        # get mean vector for each features
+        # get all distinct labels
+        self.setup(train, target)
 
         # find how many tuples for each label
         # and split train tuples based on label
-        label_tuples = {}
-        label_counts = {}
-        for l in labels:
-            label_tuples[l] = train[target == l]
-            label_counts[l] = len(label_tuples[l])
-            self.labels_prob.append(1.0 * label_counts[l] / len(target))
-            # calculate probabilities for each feature
-            self.calculate_probabilities(label_tuples[l], label_counts[l], l)
+        for l in self.labels:
+            # find all tuples under this class label
+            tuples = train[target == l]
+            # get the count
+            counts = len(tuples)
+            # calculate priors
+            self.priors[l] = 1.0 * counts / len(target)
+            # calculate likelihoods for each feature on current label
+            self.calculate_likelihoods(tuples, counts, l)
         return self
 
-    def calculate_probabilities(self, data, label_count, label):
-        self.probabilities[label] = []
+    def calculate_likelihoods(self, data, label_count, label):
+        self.likelihoods[label] = []
         for f in range(data.shape[1]):
-            feature_vals = data[:, f]
-            less_than_mean = len(feature_vals[feature_vals <= self.overall_mean[f]])
-            greater_than_mean = len(feature_vals[feature_vals > self.overall_mean[f]])
+            feature_values = data[:, f]
+            less_than_mean = len(feature_values[feature_values <= self.overall_mean[f]])
+            greater_than_mean = len(feature_values[feature_values > self.overall_mean[f]])
             pr_less_than_mean = 1.0 * (less_than_mean + 1) / (2 + label_count)
             pr_greater_than_mean = 1.0 * (greater_than_mean + 1) / (2 + label_count)
-            self.probabilities[label].append((pr_less_than_mean, pr_greater_than_mean))
+            self.likelihoods[label].append((pr_less_than_mean, pr_greater_than_mean))
 
     def predict(self, test):
         predicts = []
@@ -43,14 +49,11 @@ class BernoulliNaiveBayes:
         for t in test:
             res = []
             for l in self.labels:
-                probs = self.probabilities[l]
-                prob = 1.0
+                likelihoods = self.likelihoods[l]
+                posterior = 1.0
                 for f in range(test.shape[1]):
-                    if t[f] <= self.overall_mean[f]:
-                        prob *= probs[f][0]
-                    else:
-                        prob *= probs[f][1]
-                res.append(prob * self.labels_prob[int(l)])
+                    posterior *= likelihoods[f][self.get_likelihood_index(t[f], self.overall_mean[f])]
+                res.append(posterior * self.priors[l])
             predicts.append(res)
 
         return predicts
@@ -59,6 +62,10 @@ class BernoulliNaiveBayes:
         predicts = self.predict(test)
         return np.array(map(lambda p: 1.0 if p[0] <= p[1] else 0.0, predicts))
 
+    @staticmethod
+    def get_likelihood_index(f, mean):
+        return 0 if f <= mean else 1
 
-    def get_overall_mean(self, train):
+    @staticmethod
+    def get_overall_mean(train):
         return [train[:, f].mean() for f in range(train.shape[1])]
