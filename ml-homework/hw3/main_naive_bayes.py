94ea1b6990d5df52fbfa2a0bb59ad93c2df91d10
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-10-10 10:05:51 -0400

    Added AUC to each fold

--- ml-homework/hw3/main_naive_bayes.py
+++ ml-homework/hw3/main_naive_bayes.py
@@ -18,6 +18,7 @@ def naive_bayes(c):
 
     overall_acc = 0
     overall_error = 0
+    overall_auc = 0
 
     for start, end in test_index_generator:
         k_fold_train = np.vstack((train[range(0, start)], train[range(end, train_size)]))
@@ -27,7 +28,8 @@ def naive_bayes(c):
         test_target = target[range(start, end)]
 
         cf = get_classifier(c)
-        predicts = cf.fit(k_fold_train, train_target).predict_class(test)
+        raw_predicts = cf.fit(k_fold_train, train_target).predict(test)
+        predicts = cf.predict_class(raw_predicts)
 
         print '=============Fold %s==================' % fold
         cm = confusion_matrix(test_target, predicts)
@@ -35,12 +37,19 @@ def naive_bayes(c):
         er, acc, fpr, tpr = confusion_matrix_analysis(cm)
         print 'Error rate: %f, accuracy: %f, FPR: %f, TPR: %f' % (er, acc, fpr, tpr)
 
+        roc = ROC(test_target, raw_predicts, 0, "NaN")
+        recs = roc.create_roc_records()
+        roc.plot_roc_data(recs)
+        auc = roc.auc()
+        print 'AUC: %s' % auc
+
         overall_acc += acc
         overall_error += er
+        overall_auc += auc
         fold += 1
 
     print '--------------- Result-------------------'
-    print 'Overall Accuracy: %s, Overall Error: %s\n' % (overall_acc/k, overall_error/k)
+    print 'Overall Accuracy: %s, Overall Error: %s, Overall AUC: %s\n' % (overall_acc/k, overall_error/k, overall_auc/k)
 
 
 def get_classifier(c):
@@ -50,6 +59,7 @@ def get_classifier(c):
         return GaussianNaiveBayes()
     if c == 'histogram':
         return HistogramNaiveBayes()
+    return NBinsHistogramNaiveBayes(c)
 
 
 def gaussian_naive_bayes():
@@ -62,6 +72,10 @@ def bernoulli_naive_bayes():
 
 def histogram_naive_bayes():
     naive_bayes('histogram')
+
+
+def n_bins_histogram_naive_bayes():
+    naive_bayes(9)
 
 
 def gaussian_naive_bayes1():
@@ -77,4 +91,5 @@ def gaussian_naive_bayes1():
 if __name__ == '__main__':
     # bernoulli_naive_bayes()
     # gaussian_naive_bayes()
-    histogram_naive_bayes()
+    # histogram_naive_bayes()
+    n_bins_histogram_naive_bayes()
